59f0522fa5f97f763372b33ab27ead087d0fcc7b
Ugur Akin <ugurakinsoftware@gmail.com>
2022-05-24 23:10:11 +0300

    Modified default scope to exclude timestamps

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -4,6 +4,11 @@ const db = new Sequelize('database', '', '', {
   dialect: 'sqlite',
   storage: 'database.db',
   logging: false,
+  define: {
+    defaultScope: {
+      attributes: { exclude: ['createdAt', 'updatedAt'] },
+    },
+  },
 });
 
 module.exports = db;
